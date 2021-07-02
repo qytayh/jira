043f4327845674e9d90f7fffa326f364b8da0512
mac <mac@mac.local>
2021-07-02 17:37:07 +0800

    用状态提升分享组件状态,完成工程列表

---
+++ src/screens/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+import * as qs from 'qs'
+import {cleanObject}  from '../utils'
+import {SearchPanel} from './search-panel'
+import {List} from './list'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = ()=>{
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+  const [param,setParam] = useState({
+    name:'',
+    personId:''
+  })
+
+  useEffect(()=>{
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res=>{
+      if(res.ok){
+        setList(await res.json())
+      }
+    })
+  },[param])
+
+  useEffect(()=>{
+    fetch(`${apiUrl}/users`).then(async res=>{
+      if(res.ok){
+        setUsers(await res.json())
+      }
+    })
+  },[])
+
+  return <div>
+    <SearchPanel param={param} setParam={setParam} users={users} />
+    <List list={list} users={users} />
+  </div>
+}
