59186e598fb5d3bc64c5e9b4d6aa3a2743be8176
mac <mac@mac.local>
2021-07-16 10:27:40 +0800

    使用styled封装Row组件

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,41 @@
+import styled from "@emotion/styled";
 import React from "react";
+import { Row } from "./components/lib";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight><button onClick={logout}>登出</button></HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`
+
+const Header = styled(Row)``
+
+const HeaderLeft  = styled(Row)``
+
+const HeaderRight  = styled.div``
+
+const Main = styled.main`
+`
